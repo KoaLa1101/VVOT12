142b5e072c714a8ce7a6d20b547aa6483b7fbe50
Ахметханов Ринат <57175424+KoaLa1101@users.noreply.github.com>
2022-12-20 19:29:01 +0300

    init

---
+++ index.py
@@ -0,0 +1,97 @@
+from PIL import Image
+import requests
+from io import BytesIO
+import boto3
+import json
+import os
+import ydb
+from sanic import Sanic
+from sanic.response import text
+import sanic.response as response
+
+app = Sanic(__name__)
+
+
+@app.after_server_start
+async def after_server_start(app, loop):
+    print(f"App listening at port {os.environ['PORT']}")
+
+
+@app.route("", methods=['GET', 'POST'])
+async def hello(request):
+    bucket = 'itis-2022-2023-vvot12-photos'
+    new_bucket = 'itis-2022-2023-vvot12-faces'
+    ip = request.headers["X-Forwarded-For"]
+
+    json_msg = json.loads(request.json['messages'][0]['details']['message']['body'])
+    photo = json_msg['object_id']
+
+
+    x1 = int(json_msg['vertices'][0]['x'])
+    y1 = int(json_msg['vertices'][0]['y'])
+    x2 = int(json_msg['vertices'][2]['x'])
+    y2 = int(json_msg['vertices'][2]['y'])
+
+    url = 'https://storage.yandexcloud.net/' + bucket + '/' + photo
+    r = requests.get(url)
+    im = Image.open(BytesIO(r.content))
+    im.crop((x1, y1, x2, y2)).save(photo, quality=95)
+    session = boto3.session.Session(region_name='ru-central1')
+    s3 = session.client(
+        aws_access_key_id='YCAJELybOl5H0VpOYrRT2nTHD',
+        aws_secret_access_key='YCOXlSqjnxH3NcAJZA7i_OYEYI-vsVB8Yo6FZZbJ',
+        service_name='s3',
+        endpoint_url='https://storage.yandexcloud.net',
+        region_name='ru-central1'
+    )
+    face = str(request.json['messages'][0]['details']['message']['message_id']) + str(photo)
+    s3.upload_file(photo, new_bucket, face)
+    #Write to ydb
+    ydb_client = session.client('dynamodb', endpoint_url='https://docapi.serverless.yandexcloud.net/ru-central1/b1g71e95h51okii30p25/etnk5vgmg1pikobolq4o', aws_access_key_id='YCAJELybOl5H0VpOYrRT2nTHD', aws_secret_access_key='YCOXlSqjnxH3NcAJZA7i_OYEYI-vsVB8Yo6FZZbJ')
+    #table = ydb_client.create_table(
+    #  TableName='cloudlab',
+    #  KeySchema=[
+    #      {
+    #      'AttributeName': 'id',
+    #       'KeyType': 'HASH'
+    #      }
+    #  ],
+    #  AttributeDefinitions=[
+    #      {
+    #      'AttributeName': 'id',
+    #      'AttributeType': 'S'
+    #      },
+    #      {
+    #      'AttributeName': 'photo',
+    #      'AttributeType': 'S'
+    #      },
+    #      {
+    #      'AttributeName': 'face',
+    #      'AttributeType': 'S'
+    #      },
+    #      {
+    #      'AttributeName': 'name',
+    #      'AttributeType': 'S'
+    #      }
+    #      ]
+    #  )
+    ydb_client.put_item(TableName='cloudlab', Item={
+        'id': {
+            'S': str(request.json['messages'][0]['details']['message']['message_id']) },
+        'name': { 
+                 'S': ''},
+        'photo': {
+            'S': photo},
+        'face': {
+            'S': face}
+                                 })
+
+
+    return response.json(
+        {'message': "Handled."},
+        status=200
+    )
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ['PORT']), motd=False, access_log=False)
